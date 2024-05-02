2cbd210787da7c61fb619474dea7fd3881d93851
Ayo <ayo@metatheory.gg>
2024-05-03 00:14:19 +0800

    change to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="check-todo",
-    version="1.0.0",
+    version="1.0.1",
     py_modules=["check_todo"],
     entry_points={
         "console_scripts": [
