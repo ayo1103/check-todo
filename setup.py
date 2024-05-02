909d8df726e21a5570d6b253481910da72a76163
Ayo <ayo@metatheory.gg>
2024-05-03 00:19:59 +0800

    update

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="check-todo",
-    version="1.0.2",
+    name="check-todo-hook",
+    version="1.0.3",
     py_modules=["check_todo"],
+    install_requires=["pre-commit"],
     entry_points={
         "console_scripts": [
             "check-todo=check_todo:main",
         ],
     },
-    author="Ayo Lin",
-    author_email="ayo1103@gmail.com",
-    description="Checks for 'TODO' in staged files before committing.",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A pre-commit hook to check for TODO comments in staged files.",
 )
